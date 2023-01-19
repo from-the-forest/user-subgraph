442770ec92768125f34139ff0b5a7f9ffe2edf52
Joseph Cuffney <josephcuffney@gmail.com>
2023-01-19 16:45:36 -0500

    cleanup

--- graph/lib/user.go
+++ graph/lib/user.go
@@ -7,9 +7,9 @@ import (
 func FindUserByID(id string) (*model.User, error) {
 	user := &model.User{
 		ID:        "1",
-		FirstName: "Joe",
+		FirstName: "Nox",
 		LastName:  "Cuffney",
-		Email:     "josephcuffney@gmail.com",
+		Email:     "noxman@gmail.com",
 	}
 	return user, nil
 }
