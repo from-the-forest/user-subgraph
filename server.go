ef74a6934a3998ec357c9563f936a52c66e3d05f
Joseph Cuffney <josephcuffney@gmail.com>
2023-01-20 13:31:21 -0500

    allow studio via CORs

--- server.go
+++ server.go
@@ -34,6 +34,23 @@ func playgroundHandler() gin.HandlerFunc {
 	}
 }
 
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "https://studio.apollographql.com")
+		// c.Header("Access-Control-Allow-Credentials", "true")
+		// c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		// c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		// if c.Request.Method == "OPTIONS" {
+		//     c.AbortWithStatus(204)
+		//     return
+		// }
+
+		c.Next()
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -50,6 +67,7 @@ func main() {
 
 	// add middleware
 	r.Use(c.ContextMiddleware())
+	r.Use(CorsMiddleware())
 
 	// add route handlers
 	r.GET("/v1/graphql/user", graphqlHandler())
